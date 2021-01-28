5320a6383443413493a964ef89675443bd1dc51e
Tarek Mahmud1 <tarekmahmud319@gmail.com>
2021-01-28 07:40:15 +0600

    end successfull project

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# tarek-mahmud-s-bank
+live site: https://tarek421.github.io/tarek-mahmud-s-bank/index.html
---
+++ app.js
@@ -0,0 +1,47 @@
+//Login Area
+const login = document.getElementById('loginBtn');
+login.addEventListener('click', function(){
+    const loginArea = document.getElementById('login-area');
+    loginArea.style.display = 'none';
+    const transactionArea = document.getElementById('transaction-area');
+    transactionArea.style.display = 'block';
+})
+//Transection area
+
+const dipositeButton = document.getElementById('dipositeBtn');
+dipositeButton.addEventListener('click', function(){
+    const depositeAmount = document.getElementById('depositeAmount').value;
+    const depositeAmountNumber = parseFloat(depositeAmount);
+
+    addSpanText('current-deposite', depositeAmountNumber);
+    addSpanText('currentBalance', depositeAmountNumber);
+
+    document.getElementById('depositeAmount').value = "";
+});
+    const withdrowBtn = document.getElementById('withdrowBtn');
+    withdrowBtn.addEventListener('click', function(){
+        getWithdrowBalance('currentWithdrow');
+        const currentBalance = document.getElementById('currentBalance').innerText;
+        const currentBalanceNumber = parseFloat(currentBalance);
+        const withdrowAmount = document.getElementById('withdrowAmount').value;
+        const withdrowAmountNumber = parseFloat(withdrowAmount);
+        const total = currentBalanceNumber - withdrowAmountNumber;
+        document.getElementById('currentBalance').innerText = total;
+        document.getElementById('withdrowAmount').value = "";
+    });
+    function getWithdrowBalance(id){
+        const currentWithdrow = document.getElementById(id).innerText;
+        const currentWithdrowNumber = parseFloat(currentWithdrow);
+        const withdrowAmount = document.getElementById('withdrowAmount').value;
+        const withdrowAmountNumber = parseFloat(withdrowAmount);
+        const total = withdrowAmountNumber + currentWithdrowNumber;
+        document.getElementById(id).innerText = total;
+    }
+
+
+function addSpanText(id, depositeAmountNumber){
+    const currentBalance = document.getElementById(id).innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+    const totalBalance = currentBalanceNumber + depositeAmountNumber;
+    document.getElementById(id).innerText = totalBalance;
+}
